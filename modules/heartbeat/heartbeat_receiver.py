38b258c717cf9d2ab2b763b7263127b8585fbaee
Daniel <danielcockingson@gmail.com>
2025-11-17 00:10:07 -0500

    Commit 3

--- modules/heartbeat/heartbeat_receiver.py
+++ modules/heartbeat/heartbeat_receiver.py
@@ -38,23 +38,38 @@ class HeartbeatReceiver:
 
         # Do any intializiation here
         self.local_logger = local_logger
+        self.status = "Connected"
+        self.missed = 0
+
+
 
     def run(
-        self,
-        local_logger: logger.Logger
-    ) -> "tuple[True, None]":
+        self
+    ) -> "tuple[bool, str]":
         """
         Attempt to recieve a heartbeat message.
         If disconnected for over a threshold number of periods,
         the connection is considered disconnected.
         """
-        self.connection.recv_match(
-            mavutil.mavlink.SYS_STATUS,
-            mavutil.mavlink.
+        message = self.connection.recv_match(
+            type = 'HEARTBEAT',
+            blocking = True,
+            timeout = 2
         )
 
+        if message == 'HEARTBEAT':
+            self.status = "Connected"
+            self.missed = 0
+            self.local_logger.info("Heartbeat message received")
 
-        return True, None
+        else:
+            ++self.missed
+            self.local_logger.warning(f"{self.missed} heartbeat(s) missed")
+            if self.missed == 5:
+                self.status = "Disconnected"
+                self.local_logger.warning("Drone Disconnected")
+
+        return True, self.status
 
 
 # =================================================================================================
